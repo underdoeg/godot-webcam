43db1444bfdd02869daa420d1722e93dea1d00f0
doemsche <doemsche@gmx.ch>
2020-09-02 16:01:46 +0200

    update for godot 3.2

---
+++ CMakeFiles/godot_webcam.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dschlaepfer/projects/stapferhaus/geschlecht/godot-webcam/src/init.cpp" "/home/dschlaepfer/projects/stapferhaus/geschlecht/godot-webcam/CMakeFiles/godot_webcam.dir/src/init.cpp.o"
+  "/home/dschlaepfer/projects/stapferhaus/geschlecht/godot-webcam/src/webcam.cpp" "/home/dschlaepfer/projects/stapferhaus/geschlecht/godot-webcam/CMakeFiles/godot_webcam.dir/src/webcam.cpp.o"
+  "/home/dschlaepfer/projects/stapferhaus/geschlecht/godot-webcam/src/webcamV4L2.cpp" "/home/dschlaepfer/projects/stapferhaus/geschlecht/godot-webcam/CMakeFiles/godot_webcam.dir/src/webcamV4L2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "V4L2_FOUND"
+  "godot_webcam_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "godot-cpp/include"
+  "godot-cpp/include/core"
+  "godot-cpp/include/gen"
+  "godot-cpp/godot_headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dschlaepfer/projects/stapferhaus/geschlecht/godot-webcam/godot-cpp/CMakeFiles/godot-cpp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
